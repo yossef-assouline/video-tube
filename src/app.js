77589f75225e84657bc8fef6568854ef46ca6e75
unknown <chipeurrr@gmail.com>
2025-01-21 00:05:16 +0200

    added health check route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
 const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -9,7 +11,12 @@ app.use(cors({
 
 
 // common middleware
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended : true , limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
+
+//import routes
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
 export { app }
