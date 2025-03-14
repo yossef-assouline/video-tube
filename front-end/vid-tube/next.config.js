1ee09371522d0629dc653d558e2c99abfda2b324
unknown <chipeurrr@gmail.com>
2025-03-14 03:55:26 +0200

    ok

--- front-end/vid-tube/next.config.js
+++ front-end/vid-tube/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Changed from 'standalone' to 'export'
+  output: 'standalone', // Change back from 'export'
   images: {
-    unoptimized: true,  // Add this for static export
     domains: [
       'localhost',
       'video-tube-d2lw.onrender.com',
