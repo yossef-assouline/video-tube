32c1696c8a47ce0e24d1e39538fb1a51e74108ee
unknown <chipeurrr@gmail.com>
2025-03-16 03:54:29 +0200

    changed auth middleware

--- back-end/src/middlewares/auth.middlewares.js
+++ back-end/src/middlewares/auth.middlewares.js
@@ -5,7 +5,7 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies?.accesstoken ;
 
     if (!token) {
       return res.status(401).json({
