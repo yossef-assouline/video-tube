32f5661aa8b66ca1fcc076c212e189fcc14bd27a
unknown <chipeurrr@gmail.com>
2025-03-18 01:20:14 +0200

    changed Cors origin 2

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,10 +8,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'video-tube-one.vercel.app'
-  ],
+  origin : true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
